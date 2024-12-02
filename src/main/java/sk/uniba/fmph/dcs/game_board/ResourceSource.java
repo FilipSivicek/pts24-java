3ab5c1dfc8b5184ea918be3bfe72dfc17958eab5
filip <filip.sivicek@gmail.com>
2024-12-02 19:22:50 +0100

    Created ResourcesSource

--- src/main/java/sk/uniba/fmph/dcs/game_board/ResourceSource.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/ResourceSource.java
@@ -6,18 +6,29 @@ import sk.uniba.fmph.dcs.stone_age.HasAction;
 import sk.uniba.fmph.dcs.stone_age.PlayerOrder;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class ResourceSource implements InterfaceFigureLocationInternal {
-    private String name;
+    private String name = "";
     private final Effect resource;
     private int count;
-    private int maxFigures;
-    private int maxFigureColors;
-    private ArrayList<PlayerOrder> figures;
+    private final int maxFigures = 7;
+    private int maxFigureColors = 4;
+
+    private HashSet<PlayerOrder> playersPlaced = new HashSet<>();
+
+    private ArrayList<PlayerOrder> figures = new ArrayList<>();
 
     public ResourceSource(final Effect resource, final int count) {
         this.resource = resource;
         this.count = count;
+    }
+
+
+    public ResourceSource(final Effect resource, final int count, final String name) {
+        this.resource = resource;
+        this.count = count;
+        this.name = name;
     }
 
     /**
@@ -30,9 +41,28 @@ public class ResourceSource implements InterfaceFigureLocationInternal {
      */
     @Override
     public boolean placeFigures(final Player player, final int figureCount) {
-        return false;
-    }
+        if (player.playerOrder().getPlayers() == 3){
+            maxFigureColors = 2;
+        }
+        else if (player.playerOrder().getPlayers() == 2){
+            maxFigureColors = 1;
+        }
 
+        if (figures.size() + figureCount > maxFigures){
+            return false;
+        }
+
+        if (playersPlaced.size() >= maxFigureColors && !playersPlaced.contains(player.playerOrder())){
+            return false;
+        }
+
+        playersPlaced.add(player.playerOrder());
+        for (int i = 0; i < figureCount; i++){
+            figures.add(player.playerOrder());
+        }
+
+        return true;
+    }
     /**
      * TODO.
      *
@@ -43,7 +73,22 @@ public class ResourceSource implements InterfaceFigureLocationInternal {
      */
     @Override
     public HasAction tryToPlaceFigures(final Player player, final int count) {
-        return null;
+        if (player.playerOrder().getPlayers() == 3){
+            maxFigureColors = 2;
+        }
+        else if (player.playerOrder().getPlayers() == 2){
+            maxFigureColors = 1;
+        }
+
+        if (figures.size() + count > maxFigures){
+            return HasAction.NO_ACTION_POSSIBLE;
+        }
+
+        if (playersPlaced.size() >= maxFigureColors && !playersPlaced.contains(player.playerOrder())){
+            return HasAction.NO_ACTION_POSSIBLE;
+        }
+
+        return HasAction.WAITING_FOR_PLAYER_ACTION;
     }
 
     /**
@@ -57,7 +102,13 @@ public class ResourceSource implements InterfaceFigureLocationInternal {
      */
     @Override
     public ActionResult makeAction(final Player player, final Effect[] inputResources, final Effect[] outputResources) {
-        return null;
+        int size = figures.size();
+        for (int i = size-1; i >= 0; i--){
+            if (figures.get(i).getOrder() == player.playerOrder().getOrder()){
+                figures.remove(i);
+            }
+        }
+        return ActionResult.ACTION_DONE;
     }
 
     /**
@@ -81,7 +132,12 @@ public class ResourceSource implements InterfaceFigureLocationInternal {
      */
     @Override
     public HasAction tryToMakeAction(final Player player) {
-        return null;
+        for (PlayerOrder figure : figures) {
+            if (figure.getOrder() == player.playerOrder().getOrder()) {
+                return HasAction.WAITING_FOR_PLAYER_ACTION;
+            }
+        }
+        return HasAction.NO_ACTION_POSSIBLE;
     }
 
     /**
@@ -91,7 +147,11 @@ public class ResourceSource implements InterfaceFigureLocationInternal {
      */
     @Override
     public boolean newTurn() {
-        return false;
+        if (!figures.isEmpty()){
+            return false;
+        }
+        playersPlaced = new HashSet<>();
+        return true;
     }
 
     /**
@@ -101,6 +161,6 @@ public class ResourceSource implements InterfaceFigureLocationInternal {
      */
     @Override
     public String state() {
-        return "";
+        return name + " " + resource + " " + count;
     }
 }
