d37aba6b186abde41fc6ca5e7d0cbaf44be8fa2b
filip <filip.sivicek@gmail.com>
2024-12-02 21:18:36 +0100

    Created CivilizationCardDeck

--- src/main/java/sk/uniba/fmph/dcs/game_board/CivilizationCardDeck.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/CivilizationCardDeck.java
@@ -1,15 +1,33 @@
 package sk.uniba.fmph.dcs.game_board;
 
 import sk.uniba.fmph.dcs.stone_age.CivilisationCard;
+import sk.uniba.fmph.dcs.stone_age.EndOfGameEffect;
+import sk.uniba.fmph.dcs.stone_age.ImmediateEffect;
 
-import java.util.Optional;
+import java.util.*;
 
 public final class CivilizationCardDeck {
+    Random rand = new Random();
+    ArrayList<ImmediateEffect> possibleImmediateEffects = new ArrayList<>(List.of(ImmediateEffect.POINT,
+            ImmediateEffect.WOOD, ImmediateEffect.CLAY, ImmediateEffect.STONE, ImmediateEffect.GOLD, ImmediateEffect.FOOD));
+    ArrayList<EndOfGameEffect> possibleEndOfGameEffects = new ArrayList<>(List.of(EndOfGameEffect.FARMER, EndOfGameEffect.TOOL_MAKER,
+            EndOfGameEffect.BUILDER, EndOfGameEffect.SHAMAN, EndOfGameEffect.MEDICINE, EndOfGameEffect.ART, EndOfGameEffect.MUSIC,
+            EndOfGameEffect.WRITING, EndOfGameEffect.SUNDIAL, EndOfGameEffect.POTTERY, EndOfGameEffect.TRANSPORT, EndOfGameEffect.WEAVING));
+
     public Optional<CivilisationCard> getTop() {
-        return Optional.empty();
+        ArrayList<ImmediateEffect> immediateEffects = new ArrayList<>();
+        int amountOfImmediate = rand.nextInt(3) + 1;
+        for (int i = 0; i < amountOfImmediate; i++){
+            immediateEffects.add(possibleImmediateEffects.get(rand.nextInt(possibleImmediateEffects.size())));
+        }
+
+        EndOfGameEffect[] endOfGameEffects = new EndOfGameEffect[1];
+        endOfGameEffects[0] = possibleEndOfGameEffects.get(rand.nextInt(possibleEndOfGameEffects.size()));
+        CivilisationCard card = new CivilisationCard(immediateEffects.toArray(new ImmediateEffect[0]), endOfGameEffects);
+        return Optional.of(card);
     }
 
     public String state() {
-        return "TODO";
+        return "\n";
     }
 }
