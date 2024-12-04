8968c6faa611bbf8222f9b09e37253586ef3e428
filip <filip.sivicek@gmail.com>
2024-12-05 00:18:15 +0100

    Testing CivilizationCardDeck

--- src/test/java/sk/uniba/fmph/dcs/game_board/CivilizationCardDeckTest.java
+++ src/test/java/sk/uniba/fmph/dcs/game_board/CivilizationCardDeckTest.java
@@ -1,4 +1,22 @@
 package sk.uniba.fmph.dcs.game_board;
 
+import org.junit.Test;
+import sk.uniba.fmph.dcs.stone_age.CivilisationCard;
+
+import java.util.Optional;
+
 public class CivilizationCardDeckTest {
+    @Test
+    public void getTopTest(){
+        CivilizationCardDeck cdc = new CivilizationCardDeck();
+        for (int i = 0; i < 100; i++){
+            Optional<CivilisationCard> card = cdc.getTop();
+            if (i < 36){
+                assert card.isPresent();
+            }
+            else{
+                assert card.isEmpty();
+            }
+        }
+    }
 }
